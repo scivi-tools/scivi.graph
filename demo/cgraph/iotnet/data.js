69ff44a1694bacca1a217b989958de9f73dc2a80
Konstantin Ryabinin <kostya.ryabinin@gmail.com>
2018-03-30 14:24:32 +0500

    cgraph: iot net basic scale

--- demo/cgraph/iotnet/data.js
+++ demo/cgraph/iotnet/data.js
@@ -6,160 +6,160 @@ var g_data =
         "label": "D-K3PV",
         "group": 4,
         "weight": 8,
-        "building": "Building A", 
-        "hall": "Hall 3"
+        "building": 0, 
+        "hall": 2
     },
     {
         "id": 2,
         "label": "D-WG82",
         "group": 1,
         "weight": 5,
-        "building": "Building B", 
-        "hall": "Hall 2"
+        "building": 1, 
+        "hall": 1
     },
     {
         "id": 3,
         "label": "D-XFFD",
         "group": 3,
         "weight": 2,
-        "building": "Building A", 
-        "hall": "Hall 4"
+        "building": 0, 
+        "hall": 3
     },
     {
         "id": 4,
         "label": "D-XE6K",
         "group": 0,
         "weight": 5,
-        "building": "Building B", 
-        "hall": "Hall 2"
+        "building": 1, 
+        "hall": 1
     },
     {
         "id": 5,
         "label": "D-S414",
         "group": 1,
         "weight": 4,
-        "building": "Building B", 
-        "hall": "Hall 4"
+        "building": 1, 
+        "hall": 3
     },
     {
         "id": 6,
         "label": "D-WE8G",
         "group": 3,
         "weight": 7,
-        "building": "Building A", 
-        "hall": "Hall 1"
+        "building": 0, 
+        "hall": 0
     },
     {
         "id": 7,
         "label": "D-V0SH",
         "group": 3,
         "weight": 4,
-        "building": "Building B", 
-        "hall": "Hall 5"
+        "building": 1, 
+        "hall": 4
     },
     {
         "id": 8,
         "label": "D-C0X7",
         "group": 3,
         "weight": 9,
-        "building": "Building A", 
-        "hall": "Hall 4"
+        "building": 0, 
+        "hall": 3
     },
     {
         "id": 9,
         "label": "D-WF16",
         "group": 1,
         "weight": 9,
-        "building": "Building A", 
-        "hall": "Hall 1"
+        "building": 0, 
+        "hall": 0
     },
     {
         "id": 10,
         "label": "D-BPQ7",
         "group": 1,
         "weight": 4,
-        "building": "Building A", 
-        "hall": "Hall 5"
+        "building": 0, 
+        "hall": 4
     },
     {
         "id": 11,
         "label": "D-QFZ0",
         "group": 3,
         "weight": 1,
-        "building": "Building B", 
-        "hall": "Hall 4"
+        "building": 1, 
+        "hall": 3
     },
     {
         "id": 12,
         "label": "D-YHFC",
         "group": 3,
         "weight": 5,
-        "building": "Building A", 
-        "hall": "Hall 4"
+        "building": 0, 
+        "hall": 3
     },
     {
         "id": 13,
         "label": "D-XS6P",
         "group": 1,
         "weight": 7,
-        "building": "Building B", 
-        "hall": "Hall 2"
+        "building": 1, 
+        "hall": 1
     },
     {
         "id": 14,
         "label": "D-SWWP",
         "group": 2,
         "weight": 5,
-        "building": "Building A", 
-        "hall": "Hall 2"
+        "building": 0, 
+        "hall": 1
     },
     {
         "id": 15,
         "label": "D-HO4H",
         "group": 5,
         "weight": 1,
-        "building": "Building B", 
-        "hall": "Hall 3"
+        "building": 1, 
+        "hall": 2
     },
     {
         "id": 16,
         "label": "D-920B",
         "group": 4,
         "weight": 1,
-        "building": "Building B", 
-        "hall": "Hall 1"
+        "building": 1, 
+        "hall": 0
     },
     {
         "id": 17,
         "label": "D-CQE4",
         "group": 1,
         "weight": 4,
-        "building": "Building A", 
-        "hall": "Hall 5"
+        "building": 0, 
+        "hall": 4
     },
     {
         "id": 18,
         "label": "D-UKK4",
         "group": 2,
         "weight": 9,
-        "building": "Building A", 
-        "hall": "Hall 2"
+        "building": 0, 
+        "hall": 1
     },
     {
         "id": 19,
         "label": "D-EQ0S",
         "group": 4,
         "weight": 10,
-        "building": "Building B", 
-        "hall": "Hall 2"
+        "building": 1, 
+        "hall": 1
     },
     {
         "id": 20,
         "label": "D-561V",
         "group": 3,
         "weight": 6,
-        "building": "Building A", 
-        "hall": "Hall 5"
+        "building": 0, 
+        "hall": 4
     },
     ],
     edges: [
