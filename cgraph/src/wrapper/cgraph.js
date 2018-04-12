252dc117afa25d99009ac0eb5c4353da14f096bb
Konstantin Ryabinin <kostya.ryabinin@gmail.com>
2018-04-12 08:57:39 +0500

    cgraph: modules, work started

---
+++ cgraph/src/wrapper/cgraph.js
@@ -0,0 +1,105 @@
+
+window.$ = require("jquery");
+window.jQuery = $;
+require("jquery-ui/ui/core");
+require("jquery-ui/ui/widget");
+require("jquery-ui/ui/mouse");
+require("jquery-ui/ui/slider");
+require("jquery-ui/ui/tabs");
+var PIXI = require("pixi.js");
+var Split = require("split.js");
+require("chart.js");
+
+var SciViCGraph = require("../../build/scivi-cgraph.js");
+
+module.exports = CGraph;
+
+function CGraph()
+{
+}
+
+CGraph.prototype.parse = function (data)
+{
+    var parser = new SciViCGraph.Parser(data);
+    return parser.graphData;
+}
+
+CGraph.prototype.createScale = function (steps, colors, textColors, names, getValue)
+{
+    return new SciViCGraph.Scale(steps, colors, textColors, names, getValue);
+}
+
+CGraph.prototype.run = function (loc, data, scales)
+{
+    var split1 = $("<div>").html("<div id=\"scivi_cgraph_view\"></div>");
+    split1.attr("id", "scivi_cgraph_a");
+    split1.attr("class", "split split-horizontal");
+
+    var split2 = $("<div>").html(
+        "<div id=\"scivi_cgraph_tabs\">" +
+        "    <ul>" +
+        "        <li><a href=\"#scivi_cgraph_info\">" + loc["LOC_INFO"] + "</a></li>" +
+        "        <li><a href=\"#scivi_cgraph_list\">" + loc["LOC_NODES"] + "</a></li>" +
+        "        <li><a href=\"#scivi_cgraph_settings\">" + loc["LOC_SETTINGS"] + "</a></li>" +
+        "        <li><a href=\"#scivi_cgraph_stats\">" + loc["LOC_GROUPS"] + "</a></li>" +
+        "    </ul>" +
+        "    <div id=\"scivi_cgraph_info\"></div>" +
+        "    <div id=\"scivi_cgraph_list\"></div>" +
+        "    <div id=\"scivi_cgraph_settings\"></div>" +
+        "    <div id=\"scivi_cgraph_stats\"></div>" +
+        "</div>"
+    );
+    split2.attr("id", "scivi_cgraph_b");
+    split2.attr("class", "split split-horizontal");
+
+    $("body").prepend(split2);
+    $("body").prepend(split1);
+
+    var renderer = new SciViCGraph.Renderer($('#scivi_cgraph_view')[0], $('#scivi_cgraph_info')[0],
+                                            $('#scivi_cgraph_list')[0], $('#scivi_cgraph_settings')[0],
+                                            $('#scivi_cgraph_stats')[0], null,
+                                            loc);
+
+    Split(['#scivi_cgraph_a', '#scivi_cgraph_b'], {
+        gutterSize: 8,
+        cursor: 'col-resize',
+        onDrag: () => { renderer.reshape(); }
+    });
+
+    $("#scivi_cgraph_tabs").tabs({heightStyle: "fill"});
+
+    const colors = [0x00aa00,
+                    0x0000aa,
+                    0x00aaaa,
+                    0xe6194b,
+                    0x3cb44b,
+                    0x0082c8,
+                    0x911eb4,
+                    0x46f0f0,
+                    0xf032e6,
+                    0xd2f53c,
+                    0xfabebe,
+                    0x008080,
+                    0xe6beff,
+                    0xaa6e28,
+                    0xfffac8,
+                    0x800000,
+                    0xaaffc3,
+                    0x808000,
+                    0xffd8b1,
+                    0x000080,
+                    0x808080,
+                    0xff0000,
+                    0x00ff00,
+                    0x0000ff,
+                    0xff00ff,
+                    0xffff00,
+                    0x00ffff];
+
+    renderer.setInput(data, colors);
+    renderer.scaleLevels = scales;
+    renderer.sortNodesByRingScale();
+    renderer.run();
+
+    return renderer;
+}
