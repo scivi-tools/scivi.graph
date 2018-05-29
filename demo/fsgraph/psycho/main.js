c2e3563d1675bcd3059df8e150a683e6429cf74c
Сергей Пермяков <rewmad@gmail.com>
2018-05-29 18:06:10 +0500

    fsgraph: adapt psycho demo

---
+++ demo/fsgraph/psycho/main.js
@@ -0,0 +1,22 @@
+/**
+ * Сейчас по парам вида "невыделен - выделен";
+ * начиная от связи и продолжая всеми типами вершин по группам
+ */
+
+var g_colors = [
+    // link
+    0x4C57D877, 0x081973FF,
+    // node1
+    0xFFBA60FF, 0xFF6F00FF,
+    // node2
+    0xD5EAFFFF, 0x004282FF
+];
+
+function main() {
+    var controller = SciViFSGraph.main.GraphController.fromStatedJson(g_data, 'forceAtlas2');
+    var renderer = new SciViFSGraph.main.VivaWebGLRenderer(document.body);
+        
+    renderer.graphController = controller;
+    renderer.viewRules = renderer.buildDefaultView(g_colors);
+    renderer.run(1000);
+}
