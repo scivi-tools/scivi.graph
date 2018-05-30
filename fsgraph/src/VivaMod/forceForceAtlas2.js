dc00b8577334b296ef27654771f75d995363fc21
Сергей Пермяков <rewmad@gmail.com>
2018-05-30 15:03:55 +0500

    fsgraph/forceForceAtlas2: implement outboundAttractionDistribution & edgeWeightInfluence

--- fsgraph/src/VivaMod/forceForceAtlas2.js
+++ fsgraph/src/VivaMod/forceForceAtlas2.js
@@ -7,8 +7,50 @@ import * as random from 'ngraph.random';
 export default function createLayout(graph, options) {
     
     options.createSpringForce = _createSpringForce;
+    options.integrator = _integrate;
 
     return Viva.Graph.Layout.forceDirected(graph, options);
+
+    function _integrate(bodies, timeStep) {
+        var dx = 0, tx = 0,
+            dy = 0, ty = 0,
+            i,
+            max = bodies.length,
+            attrCompensation = 0;
+      
+        if (max === 0) {
+            return 0;
+        }
+      
+        for (i = 0; i < max; ++i) {
+            var body = bodies[i],
+                coeff = timeStep / body.mass;
+        
+            body.velocity.x += coeff * body.force.x;
+            body.velocity.y += coeff * body.force.y;
+            var vx = body.velocity.x,
+                vy = body.velocity.y,
+                v = Math.sqrt(vx * vx + vy * vy);
+        
+            if (v > 1) {
+                body.velocity.x = vx / v;
+                body.velocity.y = vy / v;
+            }
+        
+            dx = timeStep * body.velocity.x;
+            dy = timeStep * body.velocity.y;
+        
+            body.pos.x += dx;
+            body.pos.y += dy;
+        
+            tx += Math.abs(dx); ty += Math.abs(dy);
+            attrCompensation += body.mass;
+        }
+
+        options.outboundAttCompensation = attrCompensation / max;
+      
+        return (tx * tx + ty * ty)/max;
+    }
 }
 
 function _createSpringForce(options) {
@@ -19,7 +61,9 @@ class CustomSpringForceUpdater {
     constructor(options) {
         this._options = /** @type {any} */(merge(options, {
             springCoeff: 0.0002,
-            springLength: 80
+            springLength: 80,
+            edgeWeightInfluence: 1,
+            outboundAttractionDistribution: false,
         }));
 
         this._random = random.random(42);
@@ -36,6 +80,7 @@ class CustomSpringForceUpdater {
         let dx = body2.pos.x - body1.pos.x;
         let dy = body2.pos.y - body1.pos.y;
         let r = Math.sqrt(dx * dx + dy * dy);
+        let outAttrComp = this._options.outboundAttractionDistribution ? (this._options.outboundAttCompensation || 1) : 1;
 
         let springCt = ((!spring.coeff || spring.coeff < 0) ? this._options.springCoeff : spring.coeff);
 
@@ -46,7 +91,7 @@ class CustomSpringForceUpdater {
         }
 
         let d = r - length;
-        let coeff = (d > 0) ? (springCt * spring.weight * Math.log(1 + d) / d) : 0;
+        let coeff = (d > 0) ? (outAttrComp * springCt * Math.pow(spring.weight, this._options.edgeWeightInfluence) * Math.log(1 + d) / d) : 0;
 
         body1.force.x += coeff * dx;
         body1.force.y += coeff * dy;
@@ -55,3 +100,5 @@ class CustomSpringForceUpdater {
         body2.force.y -= coeff * dy;
     }
 }
+
+
