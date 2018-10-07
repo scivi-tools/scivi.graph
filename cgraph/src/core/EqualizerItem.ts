3bb9458673f4d4b63201026b199ffed9e6bc4bbd
Konstantin Ryabinin <kostya.ryabinin@gmail.com>
2018-10-07 16:26:32 +0500

    cgraph/equalizer: update slider labels

--- cgraph/src/core/EqualizerItem.ts
+++ cgraph/src/core/EqualizerItem.ts
@@ -6,6 +6,7 @@ namespace SciViCGraph
         private m_edgeWeight: Range;
         private m_highlight: RingScaleSegment;
         private m_div: any;
+        private m_hash: string;
 
         constructor(private m_renderer: Renderer, segment: RingScaleSegment)
         {
@@ -73,57 +74,77 @@ namespace SciViCGraph
 
         private addInterface()
         {
-            const idx = this.m_highlight.fromAngle.toString().replace(".", "-") + "_" +
-                        this.m_highlight.toAngle.toString().replace(".", "-") + "_" +
-                        this.m_highlight.radius.toString().replace(".", "-");
-            this.m_div = $("<div>", { id: "scivi_equalizer_item_" + idx });
+            this.m_hash = this.m_highlight.fromAngle.toString().replace(".", "-") + "_" +
+                          this.m_highlight.toAngle.toString().replace(".", "-") + "_" +
+                          this.m_highlight.radius.toString().replace(".", "-");
+            this.m_div = $("<div>", { id: "scivi_equalizer_item_" + this.m_hash });
             this.m_div.html("<div><div align='center'><div style='background-color: " +
                             color2string(this.m_highlight.color) +
                             "; color: " +
                             color2string(this.m_highlight.textColor) +
                             "; border-radius: 10px; margin-bottom: 10px; display: inline-block; width: 90%;'><b>" +
                             this.m_highlight.name +
-                            "</b></div><div class='scivi_remove' id='scivi_equalizer_remove_" + idx + "'>X</div></div>" +
+                            "</b></div><div class='scivi_remove' id='scivi_equalizer_remove_" + this.m_hash + "'>X</div></div>" +
 
                             "<div>" + this.m_renderer.localizer["LOC_NODES"] +
-                            ":&nbsp;<span id='scivi_node_treshold_" + idx + "'>" +
+                            ":&nbsp;<span id='scivi_node_treshold_" + this.m_hash + "'>" +
                             this.m_renderer.roundValS(this.m_nodeWeight.min, this.m_nodeWeight.step) + " .. " +
                             this.m_renderer.roundValS(this.m_nodeWeight.max, this.m_nodeWeight.step) +
                             "</span></div>" +
-                            "<div id='scivi_node_treshold_slider_" + idx +
+                            "<div id='scivi_node_treshold_slider_" + this.m_hash +
                             "' style='margin: 10px 10px 10px 5px'></div>" +
 
                             "<div>" + this.m_renderer.localizer["LOC_EDGES"] +
-                            ":&nbsp;<span id='scivi_edge_treshold_" + idx + "'>" +
+                            ":&nbsp;<span id='scivi_edge_treshold_" + this.m_hash + "'>" +
                             this.m_renderer.roundValS(this.m_edgeWeight.min, this.m_edgeWeight.step) + " .. " +
                             this.m_renderer.roundValS(this.m_edgeWeight.max, this.m_edgeWeight.step) +
                             "</span></div>" +
-                            "<div id='scivi_edge_treshold_slider_" + idx +
+                            "<div id='scivi_edge_treshold_slider_" + this.m_hash +
                             "' style='margin: 10px 10px 10px 5px'></div><hr/><br/>");
             $("#scivi_equalizer").append(this.m_div);
-            $("#scivi_node_treshold_slider_" + idx).slider({
+            $("#scivi_node_treshold_slider_" + this.m_hash).slider({
                 min: this.m_nodeWeight.min,
                 max: this.m_nodeWeight.max,
                 range: true,
                 values: [this.m_nodeWeight.min, this.m_nodeWeight.max],
                 step: this.m_nodeWeight.step,
-                slide: (event, ui) => {  } // FIXME
+                slide: (event, ui) => { this.changeNodesTreshold(ui.values); }
             });
-            $("#scivi_edge_treshold_slider_" + idx).slider({
+            $("#scivi_edge_treshold_slider_" + this.m_hash).slider({
                 min: this.m_edgeWeight.min,
                 max: this.m_edgeWeight.max,
                 range: true,
                 values: [this.m_edgeWeight.min, this.m_edgeWeight.max],
                 step: this.m_edgeWeight.step,
-                slide: (event, ui) => {  } // FIXME
+                slide: (event, ui) => { this.changeEdgesTreshold(ui.values); }
             });
-            $("#scivi_equalizer_remove_" + idx)[0].onclick = () => {
+            $("#scivi_equalizer_remove_" + this.m_hash)[0].onclick = () => {
                 this.m_div.remove();
                 this.m_renderer.removeEqualizerItem(this);
             };
             if ($("#scivi_cgraph_tabs").length)
                 $("#scivi_cgraph_tabs").tabs("option", "active", 3);
-            $("#scivi_equalizer_item_" + idx)[0].scrollIntoView();
+            $("#scivi_equalizer_item_" + this.m_hash)[0].scrollIntoView();
+        }
+
+        private changeNodesTreshold(values: number[])
+        {
+             $("#scivi_node_treshold_" + this.m_hash).text(this.m_renderer.roundValS(values[0], this.m_edgeWeight.step) +
+                                                           " .. " +
+                                                           this.m_renderer.roundValS(values[1], this.m_edgeWeight.step));
+            this.m_nodeWeight.min = values[0];
+            this.m_nodeWeight.max = values[1];
+            this.m_renderer.equalizeNodes();
+        }
+
+        private changeEdgesTreshold(values: number[])
+        {
+            $("#scivi_edge_treshold_" + this.m_hash).text(this.m_renderer.roundValS(values[0], this.m_edgeWeight.step) +
+                                                          " .. " +
+                                                          this.m_renderer.roundValS(values[1], this.m_edgeWeight.step));
+            this.m_edgeWeight.min = values[0];
+            this.m_edgeWeight.max = values[1];
+            this.m_renderer.equalizeEdges();
         }
 
         public matches(segment: RingScaleSegment): boolean
