4c1982d301f1642a5b77dad2887a7f0938b9fabf
Konstantin Ryabinin <kostya.ryabinin@gmail.com>
2018-10-08 21:07:14 +0500

    cgraph: show and hide all filtered nodes in list, closes #54

--- cgraph/src/core/List.ts
+++ cgraph/src/core/List.ts
@@ -1,60 +1,59 @@
 namespace SciViCGraph
 {
+    enum ListFilterType
+    {
+        NoFilter,
+        SubstringFilter,
+        StringFilter,
+        RegExpFilter
+    }
+
     export class List
     {
         private m_listDiv: JQuery;
         private m_nodes: Node[];
+        private m_listedNodes: Node[];
 
         constructor(private m_list: HTMLElement)
         {
             this.m_listDiv = null;
+            this.m_nodes = null;
+            this.m_listedNodes = null;
         }
 
-        private populate()
+        private matchStr(str: string, pattern: string, type: ListFilterType): boolean
+        {
+            if (pattern === null || pattern.length === 0)
+                return true;
+            else {
+                switch (type) {
+                    case ListFilterType.NoFilter:
+                        return true;
+
+                    case ListFilterType.SubstringFilter:
+                        return str.indexOf(pattern) !== -1;
+
+                    case ListFilterType.StringFilter:
+                        return str === pattern;
+
+                    case ListFilterType.RegExpFilter:
+                        return str.match(pattern) !== null;
+                }
+            }
+
+            return false;
+        }
+
+        private populate(pattern: string, type: ListFilterType)
         {
             this.m_listDiv.empty();
+            this.m_listedNodes = [];
             this.m_nodes.forEach((node) => {
-                node.postListItem(this.m_listDiv.get(0));
+                if (this.matchStr(node.label, pattern, type)) {
+                    node.postListItem(this.m_listDiv.get(0));
+                    this.m_listedNodes.push(node);
+                }
             });
-        }
-
-        private populateBySubstring(sub: string)
-        {
-            if (sub.length === 0)
-                this.populate();
-            else {
-                this.m_listDiv.empty();
-                this.m_nodes.forEach((node) => {
-                    if (node.label.indexOf(sub) !== -1)
-                        node.postListItem(this.m_listDiv.get(0));
-                });
-            }
-        }
-
-        private populateByMatch(str: string)
-        {
-            if (str.length === 0)
-                this.populate();
-            else {
-                this.m_listDiv.empty();
-                this.m_nodes.forEach((node) => {
-                    if (node.label === str)
-                        node.postListItem(this.m_listDiv.get(0));
-                });
-            }
-        }
-
-        private populateByRegExp(re: string)
-        {
-            if (re.length === 0)
-                this.populate();
-            else {
-                this.m_listDiv.empty();
-                this.m_nodes.forEach((node) => {
-                    if (node.label.match(re))
-                        node.postListItem(this.m_listDiv.get(0));
-                });
-            }
         }
 
         public buildList(nodes: Node[], renderer: Renderer)
@@ -69,14 +68,20 @@ namespace SciViCGraph
             let showAll = $("<button/>");
             showAll.html(renderer.localizer["LOC_SHOWALL"]);
             showAll.click(() => {
-                renderer.showAllNodes(true);
+                this.m_listedNodes.forEach((node) => {
+                    node.isShown = true;
+                });
+                renderer.updateNodesVisibility();
             });
             div1.append(showAll);
 
             let hideAll = $("<button/>");
             hideAll.html(renderer.localizer["LOC_HIDEALL"]);
             hideAll.click(() => {
-                renderer.showAllNodes(false);
+                this.m_listedNodes.forEach((node) => {
+                    node.isShown = false;
+                });
+                renderer.updateNodesVisibility();
             });
             div1.append(hideAll);
 
@@ -85,21 +90,21 @@ namespace SciViCGraph
             let filter = $("<input/>").attr({ type: "text" });
             filter.width("200px");
             filter.on("input", () => {
-                this.populateBySubstring(filter.val());
+                this.populate(filter.val(), ListFilterType.SubstringFilter);
             });
             div2.append(filter);
 
             let str = $("<button/>");
             str.html(renderer.localizer["LOC_FINDSTRING"]);
             str.click(() => {
-                this.populateByMatch(filter.val());
+                this.populate(filter.val(), ListFilterType.StringFilter);
             });
             div2.append(str);
 
             let regexp = $("<button/>");
             regexp.html(renderer.localizer["LOC_FINDREGEXP"]);
             regexp.click(() => {
-                this.populateByRegExp(filter.val());
+                this.populate(filter.val(), ListFilterType.RegExpFilter);
             });
             div2.append(regexp);
 
@@ -109,7 +114,7 @@ namespace SciViCGraph
             this.m_list.appendChild(div2.get(0));
             this.m_list.appendChild(this.m_listDiv.get(0));
 
-            this.populate();
+            this.populate(null, ListFilterType.NoFilter);
         }
     }
 }
