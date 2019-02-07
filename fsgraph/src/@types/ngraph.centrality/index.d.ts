d1f8abee1e1b5ad08e9a419132424fb6288a733b
Сергей Пермяков <rewmad@gmail.com>
2019-02-07 17:51:59 +0500

    fsgraph: bugfix ngraph.centrality typedefs

--- fsgraph/src/@types/ngraph.centrality/index.d.ts
+++ fsgraph/src/@types/ngraph.centrality/index.d.ts
@@ -4,10 +4,8 @@ declare interface CentralityResult {
 }
 
 declare module 'ngraph.centrality' {
-
-
-    export function betweenness(graph: Ngraph.Graph.Graph, oriented: any): CentralityResult;
-    export function closeness(graph: Ngraph.Graph.Graph, oriented: any): CentralityResult;
-    export function degree(graph: Ngraph.Graph.Graph, kind: any): CentralityResult;
-    export function eccentricity(graph: Ngraph.Graph.Graph, oriented: any): CentralityResult;
+    export function betweenness(graph: Ngraph.Graph.Graph, oriented?: boolean): CentralityResult;
+    export function closeness(graph: Ngraph.Graph.Graph, oriented?: boolean): CentralityResult;
+    export function degree(graph: Ngraph.Graph.Graph, kind?: 'in' | 'out' | 'both'): CentralityResult;
+    export function eccentricity(graph: Ngraph.Graph.Graph, oriented?: boolean): CentralityResult;
 }
