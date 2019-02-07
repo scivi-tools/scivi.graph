d6aa2975d85b45ffbd2fa431848b66191ab5d44b
Konstantin Ryabinin <kostya.ryabinin@gmail.com>
2019-02-08 02:14:04 +0500

    cgraph/cases: receptive multilinguism

---
+++ demo/cgraph/multilin/classifier.js
@@ -0,0 +1,258 @@
+var g_classifier = {
+children: [
+{
+name: "OrthCor",
+children: [
+{
+name: "CONS",
+children: [
+{
+name: "в:вь",
+klass: 15
+},
+{
+name: "д:дь",
+klass: 10
+},
+{
+name: "л:ль",
+klass: 3
+},
+{
+name: "м:мл",
+klass: 14
+},
+{
+name: "н:нь",
+klass: 24
+},
+{
+name: "р:рь",
+klass: 13
+},
+{
+name: "т:ть",
+klass: 18
+},
+{
+name: "щ:ч",
+klass: 21
+}
+]
+},
+{
+name: "VOW",
+children: [
+{
+name: "а:я",
+klass: 23
+},
+{
+name: "е:о",
+klass: 12
+},
+{
+name: "е:я",
+klass: 22
+},
+{
+name: "е:ё",
+klass: 20
+},
+{
+name: "и:ы",
+klass: 5
+},
+{
+name: "ла:ло",
+klass: 19
+},
+{
+name: "ла:оло",
+klass: 26
+},
+{
+name: "ле:еле",
+klass: 46
+},
+{
+name: "ра:оро",
+klass: 4
+},
+{
+name: "ре:ере",
+klass: 25
+},
+{
+name: "ре:ерё",
+klass: 40
+},
+{
+name: "ръ:ре",
+klass: 17
+},
+{
+name: "ръ:ро",
+klass: 16
+},
+{
+name: "ъ:о",
+klass: 11
+},
+{
+name: "ъ:у",
+klass: 9
+},
+{
+name: "ъл:ол",
+klass: 6
+},
+{
+name: "ър:ер",
+klass: 8
+},
+{
+name: "ър:ор",
+klass: 41
+},
+{
+name: "я:е",
+klass: 7
+}
+]
+}
+]
+},
+{
+name: "OrthCor_INC",
+children: [
+{
+name: "CONS_INC",
+children: [
+{
+name: "в:вь_INC",
+klass: 36
+},
+{
+name: "д:дь_INC",
+klass: 33
+},
+{
+name: "л:ль_INC",
+klass: 39
+},
+{
+name: "м:мл_INC",
+klass: 52
+},
+{
+name: "н:нь_INC",
+klass: 43
+},
+{
+name: "р:рь_INC",
+klass: 27
+},
+{
+name: "т:ть_INC",
+klass: 32
+},
+{
+name: "щ:ч_INC",
+klass: 55
+}
+]
+},
+{
+name: "VOW_INC",
+children: [
+{
+name: "а:я_INC",
+klass: 49
+},
+{
+name: "е:о_INC",
+klass: 31
+},
+{
+name: "е:я_INC",
+klass: 28
+},
+{
+name: "е:ё_INC",
+klass: 48
+},
+{
+name: "и:ы_INC",
+klass: 38
+},
+{
+name: "ла:ло_INC",
+klass: 47
+},
+{
+name: "ла:оло_INC",
+klass: 29
+},
+{
+name: "ле:еле_INC",
+klass: 44
+},
+{
+name: "ра:оро_INC",
+klass: 30
+},
+{
+name: "ре:ере_INC",
+klass: 50
+},
+{
+name: "ре:ерё_INC",
+klass: 56
+},
+{
+name: "ръ:ре_INC",
+klass: 54
+},
+{
+name: "ръ:ро_INC",
+klass: 37
+},
+{
+name: "ъ:о_INC",
+klass: 45
+},
+{
+name: "ъ:у_INC",
+klass: 34
+},
+{
+name: "ъл:ол_INC",
+klass: 53
+},
+{
+name: "ър:ер_INC",
+klass: 42
+},
+{
+name: "ър:ор_INC",
+klass: 51
+},
+{
+name: "я:е_INC",
+klass: 35
+}
+]
+}
+]
+},
+{
+name: "Time Q",
+klass: 1
+},
+{
+name: "Total Time Spent (s)",
+klass: 2
+}
+]
+}
