b48d68a75009158bde3759f29a732322a146bbbf
Сергей Пермяков <rewmad@gmail.com>
2019-03-29 00:05:51 +0500

    fsgraph: fix node filtering for multiple groups case (close #47)

--- fsgraph/src/VivaMod/ProxyGroupNodeRenderer.ts
+++ fsgraph/src/VivaMod/ProxyGroupNodeRenderer.ts
@@ -2,7 +2,12 @@ import { VivaColoredNodeRenderer } from './VivaColoredNodeRenderer';
 import { VivaRombusNodeRenderer } from './VivaRombusNodeRenderer';
 import { VivaTriangleNodeRenderer } from './VivaTriangleNodeRenderer';
 
-export const RENDERER_MAP: { [_: string]: {new (_: number): VivaGeneric.NodeProgram} } = {
+interface HookedNodeUI extends VivaGeneric.NodeUI {
+    __id_hooked?: boolean;
+    __id_per_group: number;
+}
+
+export const RENDERER_MAP: { [_: string]: { new(_: number): VivaGeneric.NodeProgram } } = {
     'circle': VivaColoredNodeRenderer,
     'romb': VivaRombusNodeRenderer,
     'triang': VivaTriangleNodeRenderer
@@ -11,8 +16,7 @@ export const RENDERER_MAP: { [_: string]: {new (_: number): VivaGeneric.NodeProg
 export class ProxyGroupNodeRenderer implements VivaGeneric.NodeProgram {
 
     private _renderers: VivaGeneric.NodeProgram[];
-    private _nodeCountPerGroup: Uint32Array;
-    private _lastInsertedNode: VivaGeneric.NodeUI[];
+    private _nodeListPerGroup: (HookedNodeUI[])[] = [];
     private _context: WebGLRenderingContext = null;
     private _w = 0;
     private _h = 0;
@@ -24,36 +28,33 @@ export class ProxyGroupNodeRenderer implements VivaGeneric.NodeProgram {
         // ща пойдут костыли космического масштаба
         // Проблема: разделяя вершины по группам, становится невозможно представлять их в виде непрерывных массивов, поскольку появляются дыры в нумерации
         // Решение: хранить внутри каждой вершины её ид внутри группы, подменять реальный ид на него и обратно при вызове любых функций
-        this._nodeCountPerGroup = new Uint32Array(this._renderers.length);
-
-        this._lastInsertedNode = new Array(this._renderers.length);
+        for (let i = 0; i < this._renderers.length; i++) {
+            this._nodeListPerGroup[i] = [];
+        }
     }
 
-    private _hookID(nodeUI) {
-
-        if (nodeUI['__id_hooked']) {
+    private _hookID(nodeUI: HookedNodeUI) {
+        if (!!nodeUI.__id_hooked) {
             throw new Error('Trying to hook nodeUI id twice!');
         }
+        const groupId: number = nodeUI.node.data.groupId;
 
-        nodeUI['__id_per_group'] = this._nodeCountPerGroup[nodeUI.node.data.groupId];
-        this._nodeCountPerGroup[nodeUI.node.data.groupId]++;
+        nodeUI.__id_per_group = this._nodeListPerGroup[groupId].length;
+        nodeUI.__id_hooked = true;
 
-        nodeUI['__id_hooked'] = true;
-
-        this._lastInsertedNode[nodeUI.node.data.groupId] = nodeUI;
+        this._nodeListPerGroup[groupId].push(nodeUI);
 
         return this._replaceID(nodeUI);
     }
 
-    private _replaceID(nodeUI) {
-        let oldID = nodeUI.id;
-
-        nodeUI.id = nodeUI['__id_per_group'];
+    private _replaceID(nodeUI: HookedNodeUI) {
+        const oldID = nodeUI.id;
+        nodeUI.id = nodeUI.__id_per_group;
         return oldID;
     }
 
     changeNodeType(nodeType: string, idx: number) {
-        const newProgram: VivaGeneric.NodeProgram  = new RENDERER_MAP[nodeType](64);
+        const newProgram: VivaGeneric.NodeProgram = new RENDERER_MAP[nodeType](64);
         const oldProgram = this._renderers[idx];
 
         // copy properties (nuts way)
@@ -86,35 +87,35 @@ export class ProxyGroupNodeRenderer implements VivaGeneric.NodeProgram {
     }
 
     position(nodeUI: VivaGeneric.NodeUI, pos: Ngraph.Graph.Position) {
-        const oldID = this._replaceID(nodeUI);
+        const oldID = this._replaceID(nodeUI as HookedNodeUI);
         this._renderers[nodeUI.node.data.groupId].position(nodeUI, pos);
         nodeUI.id = oldID;
     }
 
     createNode(nodeUI: VivaGeneric.NodeUI) {
-        const oldID = this._hookID(nodeUI);
+        const oldID = this._hookID(nodeUI as HookedNodeUI);
         this._renderers[nodeUI.node.data.groupId].createNode(nodeUI);
         nodeUI.id = oldID;
     }
 
     removeNode(nodeUI: VivaGeneric.NodeUI) {
-        const oldID = this._replaceID(nodeUI);
+        const oldID = this._replaceID(nodeUI as HookedNodeUI);
         this._renderers[nodeUI.node.data.groupId].removeNode(nodeUI);
+        
+        // HACK: a bit of replaceProperties logic
+        const nodeGroup = this._nodeListPerGroup[nodeUI.node.data.groupId];
+        // kinda splice
+        for (let i = nodeUI.id; i < nodeGroup.length - 1; i++) {
+            nodeGroup[i] = nodeGroup[i + 1];
+            nodeGroup[i].__id_per_group = i;
+        }
+        nodeGroup.length--;
+
         nodeUI.id = oldID;
-        this._nodeCountPerGroup[nodeUI.node.data.groupId]--;
     }
 
-    replaceProperties(replacedNode: VivaGeneric.NodeUI, newNode: VivaGeneric.NodeUI) {
-        if (replacedNode.node.data.groupId !== newNode.node.data.groupId) {
-            let lastNode = this._lastInsertedNode[replacedNode.node.data.groupId];
-            if (lastNode) {
-                lastNode['__id_per_group'] = replacedNode['__id_per_group'];
-            }
-        } else {
-            newNode['__id_per_group'] = replacedNode['__id_per_group'];
-        }
+    replaceProperties(_replacedNode: VivaGeneric.NodeUI, _newNode: VivaGeneric.NodeUI) {
         // not implemented
-        // this._renderers[replacedNode.node.data.groupId].replaceProperties(replacedNode, newNode);
     }
 
     updateTransform(newTransform: number[]) {
