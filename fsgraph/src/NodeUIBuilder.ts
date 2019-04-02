292571e8d0ad72382fd94001a6d9f61d1d4e2906
Сергей Пермяков <rewmad@gmail.com>
2019-04-02 23:01:29 +0500

    fsgraph: bundle-in node label style, allow to change them via TS (see #82, #83)

--- fsgraph/src/NodeUIBuilder.ts
+++ fsgraph/src/NodeUIBuilder.ts
@@ -3,18 +3,32 @@ import { VivaImageNodeUI } from './VivaImageNodeUI';
 import { getOrCreateTranslatorInstance } from './Misc/Translator';
 import * as $ from 'jquery';
 import 'jquery-ui/ui/widgets/spinner';
+// @ts-ignore
+import styles from './styles/node-label.css';
 
 const _FontSizeDiap: [number, number] = [4, 72];
+const className = 'scivi_fsgraph_node_label';
+const expectedStyleTitle = 'webpack-compiled';
+
+interface StyleDef {
+    rule: CSSStyleRule,
+    font: string;
+    defaultFont: string;
+    fallbackFontList: string;
+}
 
 export class NodeUIBuilder {
     private _fontSize: number = 16;
     private _labels: HTMLSpanElement[] = [];
     private _container: HTMLElement;
+    private _labelStyle: StyleDef | null;
 
     constructor(private _renderer: VivaWebGLRenderer) {
         this._container = _renderer._container
 
         this._buildUi();
+        this._labelStyle = this._prepareStyleClass();
+        console.log(this._labelStyle);
     }
 
     buildUI(graphics: any, node: Ngraph.Graph.Node): VivaImageNodeUI {
@@ -38,14 +52,40 @@ export class NodeUIBuilder {
         this._renderer.rerender();
     }
 
-    get fontSizeString() {
+    private _prepareStyleClass(): StyleDef {
+        const compiledStyles = ListToArray(document.styleSheets)
+            .filter((stylesheet: CSSStyleSheet) => (stylesheet.title === expectedStyleTitle)) as CSSStyleSheet[];
+        if (!compiledStyles || !compiledStyles.length) {
+            return null;
+        }
+        const targetSelector = `.${styles[className]}`;
+        const targetStyle = compiledStyles
+            .map(v => ListToArray(v.cssRules))
+            .reduce((prev, current) => [...prev, ...current])
+            .filter(rule => rule.type == CSSRule.STYLE_RULE)
+            .filter((rule: CSSStyleRule) => rule.selectorText === targetSelector) as CSSStyleRule[];
+        if (!targetStyle.length) {
+            return null;
+        }
+        const fontArray = targetStyle[0].style.fontFamily.split(',');
+        const defaultFont = fontArray[0] || 'Arial';
+
+        return {
+            font: defaultFont,
+            defaultFont: defaultFont,
+            fallbackFontList: fontArray.slice(1).join(','),
+            rule: targetStyle[0]
+        };
+    }
+
+    private get fontSizeString() {
         return `${this._fontSize}px`;
     }
 
-    _ensureLabelExists(id: string | number) {
+    private _ensureLabelExists(id: number) {
         if (!this._labels[id]) {
             let label = document.createElement('span');
-            label.classList.add('scivi_fsgraph_node_label');
+            label.classList.add(styles[className]);
             label.innerText = '--insert-text-here--';
             label.hidden = true;
             label.style.opacity = '0.85';
@@ -59,7 +99,6 @@ export class NodeUIBuilder {
 
     private _buildUi() {
         const tr = getOrCreateTranslatorInstance();
-        let baseContainer = $('#scivi_fsgraph_settings_appearance')[0];
 
         const innerContainer = document.createElement('div');
         innerContainer.id = 'scivi_fsgraph_settings_nodelabel';
@@ -74,8 +113,7 @@ export class NodeUIBuilder {
         const that = this;
         $(confirmBuuton).button({
             label: tr.apply('#apply')
-        });
-        $(confirmBuuton).click((event) => {
+        }).click((event) => {
             const realFontSize = parseInt(spinner.value);
             if ((_FontSizeDiap[0] <= realFontSize) && (_FontSizeDiap[1] >= realFontSize)) {
                 that.fontSize = realFontSize;
@@ -87,6 +125,17 @@ export class NodeUIBuilder {
         innerContainer.appendChild(document.createElement('br'));
         innerContainer.appendChild(spinner);
         innerContainer.appendChild(confirmBuuton);
-        baseContainer.appendChild(innerContainer)
+        $('#scivi_fsgraph_settings_appearance').append(innerContainer);
     }
+}
+
+function ListToArray<T>(list: {
+    length: number
+    item: (_: number) => T
+}): T[] {
+    const res = [];
+    for (let i = 0; i < list.length; i++) {
+        res[i] = list.item(i);
+    }
+    return res;
 }
