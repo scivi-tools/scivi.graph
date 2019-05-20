191f5471fe9331933927b08ecbdb5be43bae8c2a
Konstantin Ryabinin <kostya.ryabinin@gmail.com>
2019-05-20 15:03:29 +0500

    cgraph/demo: fake medical data

---
+++ demo/cgraph/med/data.js
@@ -0,0 +1,767 @@
+var g_data =
+{
+    nodes: [
+    {
+        "id": 1,
+        "label": "Doctor 1",
+        "group": 5,
+        "weight": 2,
+        "building": 1, 
+        "department": 2
+    },
+    {
+        "id": 2,
+        "label": "Doctor 2",
+        "group": 5,
+        "weight": 1,
+        "building": 0, 
+        "department": 5
+    },
+    {
+        "id": 3,
+        "label": "Doctor 3",
+        "group": 4,
+        "weight": 2,
+        "building": 0, 
+        "department": 3
+    },
+    {
+        "id": 4,
+        "label": "Doctor 4",
+        "group": 2,
+        "weight": 5,
+        "building": 0, 
+        "department": 2
+    },
+    {
+        "id": 5,
+        "label": "Doctor 5",
+        "group": 2,
+        "weight": 1,
+        "building": 1, 
+        "department": 2
+    },
+    {
+        "id": 6,
+        "label": "Doctor 6",
+        "group": 5,
+        "weight": 9,
+        "building": 0, 
+        "department": 1
+    },
+    {
+        "id": 7,
+        "label": "Doctor 7",
+        "group": 3,
+        "weight": 9,
+        "building": 1, 
+        "department": 3
+    },
+    {
+        "id": 8,
+        "label": "Doctor 8",
+        "group": 2,
+        "weight": 9,
+        "building": 0, 
+        "department": 1
+    },
+    {
+        "id": 9,
+        "label": "Doctor 9",
+        "group": 2,
+        "weight": 8,
+        "building": 0, 
+        "department": 3
+    },
+    {
+        "id": 10,
+        "label": "Doctor 10",
+        "group": 3,
+        "weight": 4,
+        "building": 0, 
+        "department": 3
+    },
+    {
+        "id": 11,
+        "label": "Doctor 11",
+        "group": 5,
+        "weight": 9,
+        "building": 1, 
+        "department": 2
+    },
+    {
+        "id": 12,
+        "label": "Doctor 12",
+        "group": 4,
+        "weight": 3,
+        "building": 1, 
+        "department": 3
+    },
+    {
+        "id": 13,
+        "label": "Doctor 13",
+        "group": 0,
+        "weight": 3,
+        "building": 1, 
+        "department": 3
+    },
+    {
+        "id": 14,
+        "label": "Doctor 14",
+        "group": 3,
+        "weight": 8,
+        "building": 0, 
+        "department": 2
+    },
+    {
+        "id": 15,
+        "label": "Doctor 15",
+        "group": 3,
+        "weight": 10,
+        "building": 1, 
+        "department": 2
+    },
+    {
+        "id": 16,
+        "label": "Doctor 16",
+        "group": 4,
+        "weight": 7,
+        "building": 0, 
+        "department": 1
+    },
+    {
+        "id": 17,
+        "label": "Doctor 17",
+        "group": 4,
+        "weight": 3,
+        "building": 1, 
+        "department": 3
+    },
+    {
+        "id": 18,
+        "label": "Doctor 18",
+        "group": 0,
+        "weight": 8,
+        "building": 1, 
+        "department": 3
+    },
+    {
+        "id": 19,
+        "label": "Doctor 19",
+        "group": 4,
+        "weight": 7,
+        "building": 0, 
+        "department": 5
+    },
+    {
+        "id": 20,
+        "label": "Doctor 20",
+        "group": 2,
+        "weight": 8,
+        "building": 0, 
+        "department": 1
+    },
+    ],
+    edges: [
+    {
+        "source": 8,
+        "target": 17,
+        "weight": 1,
+        "tooltip": "traffic: 1"
+    },
+    {
+        "source": 11,
+        "target": 19,
+        "weight": 1,
+        "tooltip": "traffic: 1"
+    },
+    {
+        "source": 10,
+        "target": 19,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 4,
+        "target": 20,
+        "weight": 4,
+        "tooltip": "traffic: 4"
+    },
+    {
+        "source": 9,
+        "target": 2,
+        "weight": 1,
+        "tooltip": "traffic: 1"
+    },
+    {
+        "source": 12,
+        "target": 14,
+        "weight": 4,
+        "tooltip": "traffic: 4"
+    },
+    {
+        "source": 7,
+        "target": 11,
+        "weight": 3,
+        "tooltip": "traffic: 3"
+    },
+    {
+        "source": 20,
+        "target": 8,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 15,
+        "target": 13,
+        "weight": 3,
+        "tooltip": "traffic: 3"
+    },
+    {
+        "source": 8,
+        "target": 10,
+        "weight": 2,
+        "tooltip": "traffic: 2"
+    },
+    {
+        "source": 3,
+        "target": 2,
+        "weight": 1,
+        "tooltip": "traffic: 1"
+    },
+    {
+        "source": 16,
+        "target": 6,
+        "weight": 4,
+        "tooltip": "traffic: 4"
+    },
+    {
+        "source": 8,
+        "target": 14,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 12,
+        "target": 19,
+        "weight": 4,
+        "tooltip": "traffic: 4"
+    },
+    {
+        "source": 17,
+        "target": 7,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 8,
+        "target": 8,
+        "weight": 2,
+        "tooltip": "traffic: 2"
+    },
+    {
+        "source": 12,
+        "target": 15,
+        "weight": 1,
+        "tooltip": "traffic: 1"
+    },
+    {
+        "source": 7,
+        "target": 14,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 7,
+        "target": 18,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 19,
+        "target": 12,
+        "weight": 3,
+        "tooltip": "traffic: 3"
+    },
+    {
+        "source": 17,
+        "target": 6,
+        "weight": 4,
+        "tooltip": "traffic: 4"
+    },
+    {
+        "source": 12,
+        "target": 7,
+        "weight": 2,
+        "tooltip": "traffic: 2"
+    },
+    {
+        "source": 8,
+        "target": 15,
+        "weight": 3,
+        "tooltip": "traffic: 3"
+    },
+    {
+        "source": 2,
+        "target": 20,
+        "weight": 3,
+        "tooltip": "traffic: 3"
+    },
+    {
+        "source": 1,
+        "target": 11,
+        "weight": 4,
+        "tooltip": "traffic: 4"
+    },
+    {
+        "source": 12,
+        "target": 13,
+        "weight": 3,
+        "tooltip": "traffic: 3"
+    },
+    {
+        "source": 17,
+        "target": 16,
+        "weight": 3,
+        "tooltip": "traffic: 3"
+    },
+    {
+        "source": 2,
+        "target": 14,
+        "weight": 4,
+        "tooltip": "traffic: 4"
+    },
+    {
+        "source": 9,
+        "target": 1,
+        "weight": 2,
+        "tooltip": "traffic: 2"
+    },
+    {
+        "source": 8,
+        "target": 5,
+        "weight": 1,
+        "tooltip": "traffic: 1"
+    },
+    {
+        "source": 19,
+        "target": 7,
+        "weight": 4,
+        "tooltip": "traffic: 4"
+    },
+    {
+        "source": 3,
+        "target": 11,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 14,
+        "target": 12,
+        "weight": 4,
+        "tooltip": "traffic: 4"
+    },
+    {
+        "source": 13,
+        "target": 10,
+        "weight": 2,
+        "tooltip": "traffic: 2"
+    },
+    {
+        "source": 18,
+        "target": 8,
+        "weight": 1,
+        "tooltip": "traffic: 1"
+    },
+    {
+        "source": 11,
+        "target": 8,
+        "weight": 2,
+        "tooltip": "traffic: 2"
+    },
+    {
+        "source": 13,
+        "target": 18,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 7,
+        "target": 4,
+        "weight": 2,
+        "tooltip": "traffic: 2"
+    },
+    {
+        "source": 13,
+        "target": 1,
+        "weight": 4,
+        "tooltip": "traffic: 4"
+    },
+    {
+        "source": 1,
+        "target": 16,
+        "weight": 3,
+        "tooltip": "traffic: 3"
+    },
+    {
+        "source": 9,
+        "target": 1,
+        "weight": 2,
+        "tooltip": "traffic: 2"
+    },
+    {
+        "source": 1,
+        "target": 11,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 8,
+        "target": 12,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 5,
+        "target": 11,
+        "weight": 4,
+        "tooltip": "traffic: 4"
+    },
+    {
+        "source": 15,
+        "target": 13,
+        "weight": 2,
+        "tooltip": "traffic: 2"
+    },
+    {
+        "source": 9,
+        "target": 3,
+        "weight": 2,
+        "tooltip": "traffic: 2"
+    },
+    {
+        "source": 13,
+        "target": 5,
+        "weight": 1,
+        "tooltip": "traffic: 1"
+    },
+    {
+        "source": 16,
+        "target": 6,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 9,
+        "target": 19,
+        "weight": 2,
+        "tooltip": "traffic: 2"
+    },
+    {
+        "source": 19,
+        "target": 3,
+        "weight": 4,
+        "tooltip": "traffic: 4"
+    },
+    {
+        "source": 1,
+        "target": 1,
+        "weight": 4,
+        "tooltip": "traffic: 4"
+    },
+    {
+        "source": 12,
+        "target": 8,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 18,
+        "target": 5,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 2,
+        "target": 10,
+        "weight": 2,
+        "tooltip": "traffic: 2"
+    },
+    {
+        "source": 4,
+        "target": 3,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 20,
+        "target": 15,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 5,
+        "target": 1,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 17,
+        "target": 19,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 6,
+        "target": 12,
+        "weight": 3,
+        "tooltip": "traffic: 3"
+    },
+    {
+        "source": 7,
+        "target": 20,
+        "weight": 3,
+        "tooltip": "traffic: 3"
+    },
+    {
+        "source": 15,
+        "target": 14,
+        "weight": 4,
+        "tooltip": "traffic: 4"
+    },
+    {
+        "source": 10,
+        "target": 20,
+        "weight": 1,
+        "tooltip": "traffic: 1"
+    },
+    {
+        "source": 2,
+        "target": 3,
+        "weight": 1,
+        "tooltip": "traffic: 1"
+    },
+    {
+        "source": 10,
+        "target": 7,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 18,
+        "target": 6,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 19,
+        "target": 10,
+        "weight": 1,
+        "tooltip": "traffic: 1"
+    },
+    {
+        "source": 2,
+        "target": 20,
+        "weight": 1,
+        "tooltip": "traffic: 1"
+    },
+    {
+        "source": 2,
+        "target": 16,
+        "weight": 2,
+        "tooltip": "traffic: 2"
+    },
+    {
+        "source": 19,
+        "target": 5,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 9,
+        "target": 20,
+        "weight": 2,
+        "tooltip": "traffic: 2"
+    },
+    {
+        "source": 16,
+        "target": 9,
+        "weight": 4,
+        "tooltip": "traffic: 4"
+    },
+    {
+        "source": 8,
+        "target": 3,
+        "weight": 4,
+        "tooltip": "traffic: 4"
+    },
+    {
+        "source": 11,
+        "target": 16,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 13,
+        "target": 18,
+        "weight": 2,
+        "tooltip": "traffic: 2"
+    },
+    {
+        "source": 11,
+        "target": 3,
+        "weight": 4,
+        "tooltip": "traffic: 4"
+    },
+    {
+        "source": 16,
+        "target": 2,
+        "weight": 4,
+        "tooltip": "traffic: 4"
+    },
+    {
+        "source": 8,
+        "target": 9,
+        "weight": 3,
+        "tooltip": "traffic: 3"
+    },
+    {
+        "source": 12,
+        "target": 9,
+        "weight": 4,
+        "tooltip": "traffic: 4"
+    },
+    {
+        "source": 15,
+        "target": 2,
+        "weight": 3,
+        "tooltip": "traffic: 3"
+    },
+    {
+        "source": 10,
+        "target": 6,
+        "weight": 3,
+        "tooltip": "traffic: 3"
+    },
+    {
+        "source": 7,
+        "target": 6,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 17,
+        "target": 11,
+        "weight": 3,
+        "tooltip": "traffic: 3"
+    },
+    {
+        "source": 14,
+        "target": 3,
+        "weight": 2,
+        "tooltip": "traffic: 2"
+    },
+    {
+        "source": 17,
+        "target": 13,
+        "weight": 2,
+        "tooltip": "traffic: 2"
+    },
+    {
+        "source": 8,
+        "target": 1,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 8,
+        "target": 2,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 18,
+        "target": 20,
+        "weight": 3,
+        "tooltip": "traffic: 3"
+    },
+    {
+        "source": 19,
+        "target": 6,
+        "weight": 3,
+        "tooltip": "traffic: 3"
+    },
+    {
+        "source": 16,
+        "target": 19,
+        "weight": 2,
+        "tooltip": "traffic: 2"
+    },
+    {
+        "source": 2,
+        "target": 13,
+        "weight": 3,
+        "tooltip": "traffic: 3"
+    },
+    {
+        "source": 3,
+        "target": 4,
+        "weight": 1,
+        "tooltip": "traffic: 1"
+    },
+    {
+        "source": 9,
+        "target": 3,
+        "weight": 2,
+        "tooltip": "traffic: 2"
+    },
+    {
+        "source": 9,
+        "target": 5,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 10,
+        "target": 20,
+        "weight": 1,
+        "tooltip": "traffic: 1"
+    },
+    {
+        "source": 17,
+        "target": 5,
+        "weight": 1,
+        "tooltip": "traffic: 1"
+    },
+    {
+        "source": 14,
+        "target": 14,
+        "weight": 2,
+        "tooltip": "traffic: 2"
+    },
+    {
+        "source": 15,
+        "target": 5,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 3,
+        "target": 4,
+        "weight": 2,
+        "tooltip": "traffic: 2"
+    },
+    {
+        "source": 17,
+        "target": 18,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    {
+        "source": 13,
+        "target": 19,
+        "weight": 5,
+        "tooltip": "traffic: 5"
+    },
+    ]
+}
