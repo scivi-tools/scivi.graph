9939311dfe43d3eac7ffd6ab14e5dea810b8917d
Иван Рожков <goldnotch@gmail.com>
2019-10-15 19:46:19 +0300

    File contains the function for creation Circle layout

---
+++ fsgraph/src/VivaMod/Layout/CircleLayout.js
@@ -0,0 +1,17 @@
+﻿import NgraphGraph from 'ngraph.graph';
+import { Node } from '../../Node';
+
+/**
+ * Creates Circle layout for a given graph.
+ *
+ * @param {Ngraph.Graph.Graph} graph which needs to be laid out
+ * @param {object} settings if you need custom settings
+ */
+export default function createLayout(graph, settings) {
+ 	if (!graph) {
+    	throw new Error('Graph structure cannot be undefined');
+  	}
+  
+  	var n = graph.getNode(5);
+  	n.label = 'ТЕСТОВАЯ НОДА';
+}
