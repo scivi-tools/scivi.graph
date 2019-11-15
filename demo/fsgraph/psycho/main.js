cbd9e3b52f191caeb24a3c41b094d2cb5262517b
Иван Рожков <goldnotch@gmail.com>
2019-11-15 15:12:06 +0300

    changed the order of instructions for correct layout

--- demo/fsgraph/psycho/main.js
+++ demo/fsgraph/psycho/main.js
@@ -15,15 +15,17 @@ var g_colors = [
 var FSGraph = SciViFSGraph.main;
 
 function main() {
-    var usedLayout = FSGraph.getParameterByName("layout") || "forceAtlas2f";
     var lang = SciViFSGraph.main.getParameterByName("lang") || "ru";
     document.documentElement.lang = lang;
     FSGraph.getOrCreateTranslatorInstance(lang).extend(g_fsgraph_loc);
 
-    var controller = FSGraph.GraphController.fromStatedJson(g_data, usedLayout);
-    var renderer = new FSGraph.VivaWebGLRenderer(document.body);
-        
-    renderer.graphController = controller;
+	var renderer = new FSGraph.VivaWebGLRenderer(document.body);
+    var usedLayout = FSGraph.getParameterByName("layout") || "CircleLayout";
+    //теперь графовое состояние не зависит от укладки
+    var controller = FSGraph.GraphController.fromStatedJson(g_data);
+    
+    //укладка строится уже на этапе подключения графа к процессу рендеринга
+    renderer.setGraphController(controller, usedLayout);
     renderer.viewRules = renderer.buildDefaultView(g_colors, ['circle', 'romb']);
     renderer.run(1000);
 }
